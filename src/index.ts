9c73355d8d875fc15925b000ac4d6467778d628b
Sean Price <sean@seanprice.io>
2022-02-20 12:11:06 -0500

    Export ActionCodeSettings from firebase

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './firebase';
 export * from './hooks';
 
-export type { AuthCredential, UserCredential } from 'firebase/auth';
+export type { AuthCredential, UserCredential, ActionCodeSettings } from 'firebase/auth';
 export { EmailAuthProvider, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
 export type { FirebaseError } from 'firebase/app';
